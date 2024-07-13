31e5cd00a0f37f5867e0c353eccce7886b3e67c4
Namrata Adhikari <namrataadhikari880@gmail.com>
2024-07-13 11:50:54 -0700

    practical1: dotnetrepo

---
+++ NET-learning/Assignment_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// Main program
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating a Petrol Car instance
+        Console.WriteLine("Below shows information about a petrol car:");
+        PetrolCar myPetrolCar = new PetrolCar
+        {
+            Brand = "Lamborghini",
+            Model = "Sesto Elemento",
+            Year = 2023,
+            Mileage = 20
+        };
+        myPetrolCar.DisplayInformation();
+        myPetrolCar.ChooseColor("black");
+
+        // Creating an Electric Car instance
+        Console.WriteLine("\nBelow shows information about an electric car:");
+        ElectricCar myElectricCar = new ElectricCar
+        {
+            Brand = "Tesla",
+            Model = "Model S",
+            Year = 2024,
+            BatteryCapacity = 100
+        };
+        myElectricCar.DisplayInformation();
+        myElectricCar.ChooseColor("blue");
+    }
+}
---
+++ NET-learning/FirstConsoleApp/E.ClassesAndObjects/Student.cs
@@ -0,0 +1,51 @@
+using System;
+
+class Student
+{    
+    // Default Constructor
+    public Student()
+    {
+
+    }
+    // Parameterized constructor
+    public Student(string n, byte rn)
+    {
+        name = n;
+        RollNumber = rn;
+    }
+
+    public Student(string n, byte rn , DateTime dob)
+    {
+        name = n;
+        RollNumber = rn;
+        Dob = dob;
+    }
+    
+    public string name;
+    byte rollNumber;
+    public byte RollNumber
+    {
+        get
+        {
+            return rollNumber;
+        }
+        set
+        {
+            if (value <= 100)
+            {
+                rollNumber = value;
+            }
+        }
+    }
+    public string Address { get; set; }      //Auto implemented property
+    public DateTime Dob { get; }
+
+
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Student Name: {name}");
+        Console.WriteLine($"Student Roll Number: {rollNumber}");
+        Console.WriteLine($"Student Dob: {Dob}");
+        Console.WriteLine($"Student Address: {Address}");
+    }
+}
